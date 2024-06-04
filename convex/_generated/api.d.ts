0766b00138e1664fa0df12269716eb017b2f7274
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-04 10:44:53 +0700

    setup models and mutation

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,8 +14,12 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as products from "../products.js";
-import type * as tasks from "../tasks.js";
+import type * as clerk from "../clerk.js";
+import type * as conversations from "../conversations.js";
+import type * as http from "../http.js";
+import type * as messages from "../messages.js";
+import type * as openai from "../openai.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,8 +30,12 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  products: typeof products;
-  tasks: typeof tasks;
+  clerk: typeof clerk;
+  conversations: typeof conversations;
+  http: typeof http;
+  messages: typeof messages;
+  openai: typeof openai;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
