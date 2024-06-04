5b4eaee710d0eb72ac36bf9d2ebb5eb9138e544e
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-04 14:26:47 +0700

    conditional rendering left-panel

--- src/components/home/LeftPanel.tsx
+++ src/components/home/LeftPanel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ListFilter,
   LogOut,
@@ -11,8 +13,11 @@ import { conversations } from "@/dummy-data/db";
 import Conversation from "./Conversation";
 import { UserButton } from "@clerk/nextjs";
 import UserListDialog from "./UserListDialog";
+import { useConvexAuth } from "convex/react";
 
 const LeftPanel = () => {
+  const { isAuthenticated } = useConvexAuth();
+
   return (
     <div className="w-1/4 border-gray-600 border-r">
       <div className="sticky top-0 bg-left-panel z-10">
@@ -21,7 +26,7 @@ const LeftPanel = () => {
           <UserButton />
 
           <div className="flex items-center gap-3">
-            <UserListDialog />
+            {isAuthenticated && <UserListDialog />}
             <ThemeSwitch />
           </div>
         </div>
